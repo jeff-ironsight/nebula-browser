402ca0e937e9c1a16730b630fcd3b097587757cf
jeff-ironsight <jeff@ironsight.app>
2026-01-30 01:17:13 -0700

    build(tsconfig): add vite tsconfig paths package for automatic tsconfig to vite translation

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), tsconfigPaths()],
   server: {
     proxy: {
       '/api': {
