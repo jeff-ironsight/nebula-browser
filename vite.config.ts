5f4764543e52881063fad0ef448559124203997d
jeff-ironsight <jeff@ironsight.app>
2026-01-30 06:58:09 -0700

    style(ui): add multiple shadcn components to project files be used in the future

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import tsconfigPaths from "vite-tsconfig-paths";
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), tsconfigPaths()],
+  plugins: [vue(), tailwindcss(), tsconfigPaths({ projects: ["tsconfig.app.json"] })],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   server: {
     proxy: {
       '/api': {
